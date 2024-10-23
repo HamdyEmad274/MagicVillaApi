9ea3da020c1d7146f5a4659e37afd3a07b214598
Hamdy Emad <hamdyemad274@gmail.com>
2024-10-23 11:48:48 +0300

    Added First EndPoint

---
+++ MagicVillaApi/Controllers/VillaApiController.cs
@@ -0,0 +1,23 @@
+﻿using MagicVillaApi.Data;
+using MagicVillaApi.Models;
+using MagicVillaApi.Models.Dto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVillaApi.Controllers
+{
+    [Route("api/VillaApi")]
+    [ApiController]
+    public class VillaApiController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<VillaDTO> GetVillas()
+        {
+            return VillaStore.VillaList;
+        }
+        [HttpGet("{id:int}")]
+        public VillaDTO GetVilla(int id)
+        {
+            return VillaStore.VillaList.Find(x=>x.Id==id);
+        }
+    }
+}
---
+++ MagicVillaApi/Data/VillaStore.cs
@@ -0,0 +1,13 @@
+﻿using MagicVillaApi.Models.Dto;
+
+namespace MagicVillaApi.Data
+{
+    public class VillaStore
+    {
+        public static List<VillaDTO> VillaList = new List<VillaDTO>()
+        {
+            new VillaDTO{ Id=1, Name="Pool View"},
+            new VillaDTO{ Id=2, Name="Beach View"},
+        };
+    }
+}
